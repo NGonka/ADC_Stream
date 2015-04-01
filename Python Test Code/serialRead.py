3e3155f4584d0f0d758963e5f6075dbe1963856e
NGonka <ngonka@gmx.de>
2015-04-01 15:55:29 +0200

    Setup for Measurement board
    plus some time testing

--- Python Test Code/serialRead.py
+++ Python Test Code/serialRead.py
@@ -5,7 +5,7 @@ import serial,json,time,csv,sys
 
 
 
-ser = serial.Serial('COM4',115200)
+ser = serial.Serial('COM11',115200)
 
 message=''
 
@@ -54,56 +54,58 @@ def validate_json(text):
     except:
         return False
 with open ('Data.csv','w', newline='') as csvfile:
-    fieldnames=['voltage','current','power','rpm','timestamp']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    while ser.isOpen():
-        # print(timestamp)
-        # print(ser.isOpen())
-        
-        # if ser.readline() is not 'EOL':
-            # message+=str(ser.readline())
-            # print(message)
-        message = readlineCR(ser)
-        # print('Message: {0}'.format(message))
-        # print('count open: {0} count close: {1}'.format(message.count('{'),message.count('}')))
-        if message.count('{')==1 & message.count('}')==1 :
-            try:
-                text= json.loads(message)
-                if validate_json(text):
-                    timestamp=int(text['timestamp'])
-                    # print(text['timestamp'])
-                    if timestamp>0:             #2*60*1000000:
-                        writer.writerow(text)
-                else:
-                    message=''
-                    # timestamp=0
-                    ser.flushOutput()
-            except ValueError as err:
-                print('JSON Error')
-                print (err)
-                print('Faulty Message: {0}'.format(message))
-                message=''
-                # timestamp=0
-                ser.flushOutput()
-                pass
-        else:
-            message=''
-            # timestamp=0
-            ser.flushOutput()
-        
-        if timestamp>4*60*1000000:
-            ser.write(b'S')
-            sys.exit()
+	print(time.strftime('%X %x'))
+	fieldnames=['voltage','current','power','rpm','timestamp']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+	while ser.isOpen():
+		# print(timestamp)
+		# print(ser.isOpen())
+		
+		# if ser.readline() is not 'EOL':
+			# message+=str(ser.readline())
+			# print(message)
+		message = readlineCR(ser)
+		# print('Message: {0}'.format(message))
+		# print('count open: {0} count close: {1}'.format(message.count('{'),message.count('}')))
+		if message.count('{')==1 & message.count('}')==1 :
+			try:
+				text= json.loads(message)
+				if validate_json(text):
+					timestamp=int(text['timestamp'])
+					# print(text['timestamp'])
+					if timestamp>0:             #2*60*1000000:
+						writer.writerow(text)
+				else:
+					message=''
+					# timestamp=0
+					ser.flushOutput()
+			except ValueError as err:
+				print('JSON Error')
+				print (err)
+				print('Faulty Message: {0}'.format(message))
+				message=''
+				# timestamp=0
+				ser.flushOutput()
+				pass
+		else:
+			message=''
+			# timestamp=0
+			ser.flushOutput()
+		
+		if timestamp>4*60*1000000:
+			print(time.strftime('%X %x'))
+			ser.write(b'S')
+			sys.exit()
 
-        
-        
-        # diff=(timestamp-timestamp_last)
-        # diff_diff=abs(diff-diff_last)
-        # freq=1/(diff/1000000)
-        # print(round(freq,2))
+		
+		
+		# diff=(timestamp-timestamp_last)
+		# diff_diff=abs(diff-diff_last)
+		# freq=1/(diff/1000000)
+		# print(round(freq,2))
 
-        # print(diff)
-        # timestamp_last=timestamp
-        # diff_last=diff
-    
+		# print(diff)
+		# timestamp_last=timestamp
+		# diff_last=diff
+
